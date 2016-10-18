74ce3269e3cef702c520707ad0a01efecd2068e0
AloesLu <785080572@qq.com>
2016-10-18 10:12:01 +0800

    Initial Commit

---
+++ LWModuleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWModuleDemo
+//
+//  Created by AloesLu on 2016/10/18.
+//  Copyright © 2016年 AloesLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
